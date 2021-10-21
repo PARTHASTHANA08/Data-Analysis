3b1eadf8bad28806b89c28bd80f78b3fde75b7e1
PARTHASTHANA08 <73220883+PARTHASTHANA08@users.noreply.github.com>
2021-10-21 15:41:40 +0530

    Update project.py

--- project.py
+++ project.py
@@ -4,6 +4,6 @@ import plotly.graph_objects as go
 import plotly.express as px
 
 df = pd.read_csv('attempts.csv')
-mean = df.groupby(["student_id","level"])["attempt"].mean()
+mean = df.groupby(["student_id","level"], as_index=False)["attempt"].mean()
 fig = px.scatter(mean, x = "student_id" , y = "level",size="attempt",color = "attempt")
 fig.show()
